c4e789f773ec7bef65e4c176835bf406c8101495
Crowdbotics <team@crowdbotics.com>
2023-10-22 01:06:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crimson-butterfly-44088",
+  url: "https://crimson-butterfly-44088.botics.co",
+  api: "https://crimson-butterfly-44088.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
